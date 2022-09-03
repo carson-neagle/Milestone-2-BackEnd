6bf7341592f0fdf7adfe4ee2dda20e6a813d1c22
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-09-03 10:45:18 -0400

    Cleaned up starter code
    For some reason I am getting an error when trying to run on a local server. but stater code is cleaned up a bit

---
+++ app.js
@@ -0,0 +1,23 @@
+//Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+require("dotenv").config();
+
+//Initialize the app object
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+const PORT = process.env.PORT || 3000;
+// routes
+const routes = require("./server/routes/recipeRoutes.js");
+app.use("/", routes);
+
+app.listen(port, () => console.log(`listening on port ${port}`));
