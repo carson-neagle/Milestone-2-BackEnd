7bdd35fe0b46001b488ff4581eb83db866298116
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-09-05 10:39:37 -0400

    category schema created

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({});
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "this field is required.",
+  },
+  image: {
+    type: String,
+    required: "this field is required.",
+  },
+});
 
 module.exports = mongoose.model("Category", categorySchema);
--- server/models/database.js
+++ server/models/database.js
@@ -13,3 +13,5 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
   console.log("Connected");
 });
+// Models
+require("/.Category");
