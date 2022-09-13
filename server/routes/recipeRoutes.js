afeed46622c62b34a84e94c3948a0b623dad2fa3
carsonneagle <neagle.carson@gmail.com>
2022-09-13 11:43:02 -0400

    Update recipeRoutes.js

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -3,5 +3,8 @@ const router = express.router();
 const recipeController = require("../controllers/recipeController");
 
 router.get("/", recipeController.homepage);
+router.get("/recipe/:id", recipeController.exploreRecipe);
+router.post("/search", recipeController.searchRecipe);
+router.get("/categories", recipeController.exploreCategories);
 
 module.exports = router;
