3006aaba873f52eb06f8e3c8f700c2dd00ba09f3
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-09-13 19:29:21 -0400

    seed change

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,73 +1,71 @@
-const router = require('express').Router()
-const places = require("../models/recipes")
+const router = require("express").Router();
+const places = require("../models/Recipes");
 
-exports.homepage = async(req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-        // Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.ingredients) {
-        req.body.ingredients = 'Ingredients'
-    }
-    if (!req.body.instructions) {
-        req.body.instructions = 'Instructions'
-    }
-    places.push(req.body)
-    res.redirect('/recipes')
-}
-
-
-exports.homepage = async(req, res) => {
-    res.render('recipes/index', { recipes })
+exports.homepage = async (req, res) => {
+  console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.ingredients) {
+    req.body.ingredients = "Ingredients";
+  }
+  if (!req.body.instructions) {
+    req.body.instructions = "Instructions";
+  }
+  places.push(req.body);
+  res.redirect("/recipes");
 };
 
-
-exports.newRecipe = async(req, res) => {
-    res.render('recipes/new')
+exports.homepage = async (req, res) => {
+  res.render("recipes/index", { recipes });
 };
 
-exports.getRecipeById = async(req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    } else if (!recipes[id]) {
-        res.render('error404')
-    } else {
-        res.render('recipes/show', { recipe: recipes[id], id })
+exports.newRecipe = async (req, res) => {
+  res.render("recipes/new");
+};
+
+exports.getRecipeById = async (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!recipes[id]) {
+    res.render("error404");
+  } else {
+    res.render("recipes/show", { recipe: recipes[id], id });
+  }
+};
+
+exports.deleteRecipe = async (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    recipes.splice(i, 1);
+    res.redirect("/recipes");
+  }
+};
+
+exports.updateRecipe = async (req, res) => {
+  try {
+    const recipes = await recipes.findById(req.params.id);
+    if (!recipes) {
+      return next(new AppError("No recipes found with that ID", 404));
     }
+
+    res.render("edit", {
+      name: name,
+      type: type,
+      ingredients: ingredients,
+      category: category,
+      instructions,
+      instructions,
+    });
+  } catch (err) {
+    res.status(404).render("error");
+  }
 };
 
-exports.deleteRecipe = async(req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    } else if (!places[id]) {
-        res.render('error404')
-    } else {
-        recipes.splice(i, 1)
-        res.redirect('/recipes')
-    }
-};
-
-exports.updateRecipe = async(req,res) => {
-    try {
-        const recipes = await recipes.findById(req.params.id);
-        if(!recipes) {
-          return next(new AppError('No recipes found with that ID', 404));
-        }
-    
-        res.render('edit', {
-          name: name,
-          type: type,
-          ingredients: ingredients,
-          category: category,
-          instructions, instructions,
-        });
-      }
-      catch(err) {
-        res.status(404).render('error')
-      }
-}
-
-module.exports = router
+module.exports = router;
