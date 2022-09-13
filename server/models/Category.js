4e688da33957cef249cb9fdad19f2a38ff428735
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-09-13 12:11:44 -0400

    category schema complete

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,5 +1,32 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const categorySchema = new mongoose.Schema({});
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "This field is required",
+  },
+  type: {
+    type: String,
+    enum: ["Entree", "Appetizer", "Dessert"],
+    required: "This field is required.",
+  },
+  ingredients: {
+    type: Array,
+    required: "This field is required.",
+  },
+  category: {
+    type: String,
+    enum: ["American", "Mexican", "Asian", "Greek"],
+    required: "This field is required.",
+  },
+  instructions: {
+    type: Array,
+    required: "This field is required.",
+  },
+  image: {
+    type: String,
+  },
+});
 
 module.exports = mongoose.model("Category", categorySchema);
